3079f4f230cd16e0eece8de205a3940c8eac37f5
Kaiky Leite <kaiky.leite@estudante.ifgoiano.edu.br>
2025-05-02 17:42:08 -0300

    Classe carro
    Criada a classe carro que herda da classe veiculo

---
+++ LocadoraVeiculos/src/main/java/com/mycompany/locadoraveiculos/Carro.java
@@ -0,0 +1,66 @@
+package com.mycompany.locadoraveiculos;
+
+public class Carro extends Veiculo {
+
+    private int numPortas;
+    private String tipoCombustivel;
+    private boolean arCondicionado;
+    private String cambio;
+
+    public Carro(int id, String modelo, String placa, int ano, double precoDiario,
+            int numPortas, String tipoCombustivel, boolean arCondicionado, String cambio) {
+        super(id, modelo, placa, ano, precoDiario);
+        this.numPortas = numPortas;
+        this.tipoCombustivel = tipoCombustivel;
+        this.arCondicionado = arCondicionado;
+        this.cambio = cambio;
+    }
+
+    @Override
+    public double calcularSeguro() {
+        // Seguro para carro: 20% do preço diário + taxa fixa de R$15
+        return getPrecoDiario() * 0.20 + 15;
+    }
+
+    // Getters e Setters específicos
+    public int getNumPortas() {
+        return numPortas;
+    }
+
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public boolean isArCondicionado() {
+        return arCondicionado;
+    }
+
+    public String getCambio() {
+        return cambio;
+    }
+
+    public void setNumPortas(int numPortas) {
+        this.numPortas = numPortas;
+    }
+
+    public void setTipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    public void setArCondicionado(boolean arCondicionado) {
+        this.arCondicionado = arCondicionado;
+    }
+
+    public void setCambio(String cambio) {
+        this.cambio = cambio;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nTipo: Carro" + "\nNúmero de Portas: " + numPortas
+                + "\nCombustível: " + tipoCombustivel + "\nAr Condicionado: "
+                + (arCondicionado ? "Sim" : "Não") + "\nCâmbio: " + cambio;
+    }
+}
+
+
