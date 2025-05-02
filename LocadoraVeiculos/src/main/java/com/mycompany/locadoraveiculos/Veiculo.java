30c2e722623dec6f2a313942a8171d5146ace81d
Kaiky Leite <kaiky.leite@estudante.ifgoiano.edu.br>
2025-05-02 14:00:31 -0300

    Classe Veiculo
    criaçao da classe Veiculo

---
+++ LocadoraVeiculos/src/main/java/com/mycompany/locadoraveiculos/Veiculo.java
@@ -0,0 +1,59 @@
+package com.mycompany.locadoraveiculos;
+
+public class Veiculo {
+
+    private int id;
+    private String modelo;
+    private String placa;
+    private int ano;
+    private double precoDiario;
+
+    public Veiculo(int id, String modelo, String placa, int ano, double precoDiario) {
+        this.id = id;
+        this.modelo = modelo;
+        this.placa = placa;
+        this.ano = ano;
+        this.precoDiario = precoDiario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getPrecoDiario() {
+        return precoDiario;
+    }
+
+    public void setPrecoDiario(double precoDiario) {
+        this.precoDiario = precoDiario;
+    }
+
+}
