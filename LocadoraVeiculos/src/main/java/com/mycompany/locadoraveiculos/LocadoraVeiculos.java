802b9de574d969ebcaf116c7453a1b7c478204fe
Tiago Cardoso Ferreira <tiagocardoso1357@gmail.com>
2025-05-02 13:34:25 -0300

    Add files via upload

---
+++ LocadoraVeiculos/src/main/java/com/mycompany/locadoraveiculos/LocadoraVeiculos.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.locadoraveiculos;
+
+/**
+ *
+ * @author tiago
+ */
+public class LocadoraVeiculos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
